19b01d3e28939bc222456626239acbf6bf36a43b
edmccrea <ed-mccrea@hotmail.co.uk>
2023-12-09 13:56:57 +0100

    add types for style and toast

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,6 +1,23 @@
 declare global {
   namespace App {
-    type Style = "2d-cartoon" | "lego" | "pixar" | "anime";
+    type Style =
+      | "2d-cartoon"
+      | "lego"
+      | "pixar"
+      | "anime"
+      | "monster"
+      | "comic-book-villain"
+      | "comic-book-hero"
+      | "abstract"
+      | "watercolor"
+      | "oil-painting";
+
+    type ToastType = "info" | "warn" | "error" | "success";
+
+    interface ToastMessage {
+      type: ToastType;
+      message: string;
+    }
   }
 }
 
