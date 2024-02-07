4944625106123cf1536f33e65c92e84474c72082
edmccrea <ed-mccrea@hotmail.co.uk>
2024-02-07 22:13:55 +0100

    asynchronously make open ai call

--- src/routes/api/generate/+server.ts
+++ src/routes/api/generate/+server.ts
@@ -46,51 +46,54 @@ export const POST: RequestHandler = async ({ request }) => {
     const writer = writable.getWriter();
     writer.write(data);
 
-    const openai = new OpenAI({ apiKey: requestData.key });
-    const visionResponse = await openai.chat.completions.create({
-      model: "gpt-4-vision-preview",
-      messages: [
-        {
-          role: "user",
-          content: [
-            {
-              type: "text",
-              text: "Desribe what is in this image in great detail. Give as much detail as possible about any humans in the image, especially those in the foreground. Make sure to include how they are dressed and positioned.",
-            },
-            {
-              type: "image_url",
-              image_url: { url: requestData.image },
-            },
-          ],
-        },
-      ],
-      max_tokens: 1000,
-    });
+    async function performApiCall() {
+      const openai = new OpenAI({ apiKey: requestData.key });
+      const visionResponse = await openai.chat.completions.create({
+        model: "gpt-4-vision-preview",
+        messages: [
+          {
+            role: "user",
+            content: [
+              {
+                type: "text",
+                text: "Desribe what is in this image in great detail. Give as much detail as possible about any humans in the image, especially those in the foreground. Make sure to include how they are dressed and positioned.",
+              },
+              {
+                type: "image_url",
+                image_url: { url: requestData.image },
+              },
+            ],
+          },
+        ],
+        max_tokens: 1000,
+      });
 
-    const style: App.Style = requestData.style;
+      const style: App.Style = requestData.style;
 
-    const basePrompt = visionResponse.choices[0].message.content;
-    const stylePrompt = prompts[style];
-    const prompt = basePrompt + stylePrompt;
-    if (!prompt) {
-      throw new Error("GPT Vision failed");
+      const basePrompt = visionResponse.choices[0].message.content;
+      const stylePrompt = prompts[style];
+      const prompt = basePrompt + stylePrompt;
+      if (!prompt) {
+        throw new Error("GPT Vision failed");
+      }
+
+      const image = await openai.images.generate({
+        model: "dall-e-3",
+        prompt,
+        response_format: "b64_json",
+        quality: requestData.isHD ? "hd" : "standard",
+      });
+
+      data = encoder.encode(
+        "event: image\ndata: " +
+          JSON.stringify({ type: "image", data: image.data }) +
+          "\n\n"
+      );
+      writer.write(data);
+      writer.close();
     }
 
-    const image = await openai.images.generate({
-      model: "dall-e-3",
-      prompt,
-      response_format: "b64_json",
-      quality: requestData.isHD ? "hd" : "standard",
-    });
-
-    data = encoder.encode(
-      "event: image\ndata: " +
-        JSON.stringify({ type: "image", data: image.data }) +
-        "\n\n"
-    );
-    writer.write(data);
-    writer.close();
-
+    performApiCall();
     return new Response(readable, {
       headers: { "Content-Type": "text/event-stream" },
     });
