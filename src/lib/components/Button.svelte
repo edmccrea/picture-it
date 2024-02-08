fb26b7e89f709f22a8f3aedb20acc04100577a94
edmccrea <ed-mccrea@hotmail.co.uk>
2024-02-08 23:04:31 +0100

    update button styles

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -3,13 +3,12 @@
   import { cva, type VariantProps } from "class-variance-authority";
 
   const button = cva(
-    "px-4 py-2 duration-300 ease-in-out rounded-md w-fit text-neutral-50 hover:cursor-pointer",
+    "px-4 py-2 duration-300 ease-in-out rounded-md w-fit text-neutral-50",
     {
       variants: {
         intent: {
           primary: "bg-sky-400 hover:bg-sky-600",
-          secondary:
-            "bg-transparent border border-sky-500 hover:bg-sky-400/10 text-[#333333] ",
+          secondary: "bg-sky-900 border hover:bg-sky-900/70 text-[#333333] ",
           disabled: "bg-sky-100 hover:cursor-default",
         },
         size: {
@@ -27,12 +26,14 @@
 
   export let intent: $$Props["intent"] = "primary";
   export let size: $$Props["size"] = "medium";
+  export let type: $$Props["type"] = "button";
 </script>
 
 <button
   {...$$props}
   class={button({ intent, size, class: $$props.class })}
   on:click
+  {type}
 >
   <slot />
 </button>
