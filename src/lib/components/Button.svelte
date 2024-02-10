7d77405effb6e94ab049e82b229ed0491cd2845f
edmccrea <ed-mccrea@hotmail.co.uk>
2024-02-10 22:00:47 +0100

    update button styling and add ghost button

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -2,25 +2,23 @@
   import type { HTMLButtonAttributes } from "svelte/elements";
   import { cva, type VariantProps } from "class-variance-authority";
 
-  const button = cva(
-    "px-4 py-2 duration-300 ease-in-out rounded-md w-fit text-neutral-50",
-    {
-      variants: {
-        intent: {
-          primary: "bg-sky-400 hover:bg-sky-600",
-          secondary: "bg-sky-900 border hover:bg-sky-900/70 text-[#333333] ",
-          disabled: "bg-sky-100 hover:cursor-default",
-        },
-        size: {
-          small: "text-sm",
-          medium: "text-base",
-        },
+  const button = cva("px-4 py-2 duration-300 ease-in-out rounded-md w-fit", {
+    variants: {
+      intent: {
+        primary: "bg-sky-400 hover:bg-sky-600 text-neutral-50",
+        secondary: "bg-sky-900 border hover:bg-sky-900/70 text-neutral-50",
+        disabled: "bg-sky-100 hover:cursor-default text-neutral-50",
+        ghost: "bg-transparent  hover:bg-sky-100 text-neutral-500",
       },
-      compoundVariants: [
-        { intent: "primary", size: "medium", class: "primaryMedium" },
-      ],
-    }
-  );
+      size: {
+        small: "text-sm",
+        medium: "text-base",
+      },
+    },
+    compoundVariants: [
+      { intent: "primary", size: "medium", class: "primaryMedium" },
+    ],
+  });
 
   interface $$Props extends HTMLButtonAttributes, VariantProps<typeof button> {}
 
